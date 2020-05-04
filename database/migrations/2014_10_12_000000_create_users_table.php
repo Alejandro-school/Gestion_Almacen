393cc4c19649d049e46256142284a2544499a95e
Manel <manel.aguilera91@gmail.com>
2020-05-04 12:24:22 +0200

    creando migraciones

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+            $table->timestamps();
             $table->string('name');
+            $table->string('surname');
+            $table->integer('type_user');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
+            
         });
     }
 
