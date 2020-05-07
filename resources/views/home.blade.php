9004326e1ef279544441e150de95701e0a34e79a
Manel <manel.aguilera91@gmail.com>
2020-05-07 13:02:51 +0200

    haciendo home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,17 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-          
+            <div class="card">
+                <div class="card-header">
+                    Información
+                </div>
+                <div class="card-body">
+                    <div class="form-inline">
+                        <label style="margin-right:10px;" for="usr">Numero Interno:</label>
+                        <input type="text" class="form-control" id="usr">
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
