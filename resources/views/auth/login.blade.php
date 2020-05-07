b137679e1ed54a30026c5e7aaba8f801e35c9fd5
Manel <manel.aguilera91@gmail.com>
2020-05-07 12:31:59 +0200

    Poniendo titulos en las paginas de autenticación

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
 
+@section('titulo','Iniciar Sesión')
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
 
+@section('titulo','Registrate')
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
