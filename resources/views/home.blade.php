ef2ebc6d84dc6ac348ffddfda68b92c4aa6b4a41
Manel <manel.aguilera91@gmail.com>
2020-05-07 14:02:25 +0200

    diseño de home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,75 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-8 mb-4">
             <div class="card">
                 <div class="card-header">
-                    Información
+                    Buscar
                 </div>
                 <div class="card-body">
-                    <div class="form-inline">
-                        <label style="margin-right:10px;" for="usr">Numero Interno:</label>
+                    <div class="form-row">
+                      <div class="col">
+                        <label style="margin-right:10px;" for="usr">Codigo Interno:</label>
                         <input type="text" class="form-control" id="usr">
+                        </div>
+
+                        <div class="col">
+                        <label style="margin-right:10px;" for="usr">Codigo Producto:</label>
+                        <input type="text" class="form-control" id="usr">
+                        </div>
+                        
                     </div>
+                    <button type="submit" class="btn btn-secondary mt-3">Buscar</button>
+                    
                 </div>
             </div>
         </div>
+
+        <div class="col-md-8">
+            <div class="card">
+
+                <div class="card-body">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col">Id_producto</th>
+                                <th scope="col">Id_interno</th>
+                                <th scope="col">Proveedor</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                               
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                           
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>Larry the Bird</td>
+                                <td>Peter</td>
+                            
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+
+
+
+        </div>
+
+
     </div>
+</div>
+
+
 </div>
 @endsection
